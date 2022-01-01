5c9f04d139fbf2a1819ed8e29bfadc763e461c48
viettrungIT3 <viettrungcntt03@gmail.com>
2022-01-01 16:06:10 +0700

    BTVN buổi 1

---
+++ BTVN_1/Bai1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Ho ten: Nguyen Viet Trung.\n");
+    printf("Tuoi: 20.\n");
+    printf("Que quan: Thai Nguyen.\n");
+    printf("Lop: CNTT03-K14.\n");
+    printf("So thich: di phuot, chay bo,...\n");
+    printf("Tinh cach: than thien, hoa dong, sang tao, nhiet tinh,....\n");
+    printf("Ke hoach sap toi: hoan thanh 100% khoa hoc C/C++ 16 cua CLB Tin Hoc HIT.\n");
+    return 0;
+}
